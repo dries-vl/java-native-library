009390dd4de30b709465c6c278d0319a3a4db2f8
dries-vl <driesvanleuvenhaege@hotmail.com>
2023-09-20 23:03:06 +0200

    graalvm native java dll

---
+++ src/main/java/LibEnvMap.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+
+public class LibEnvMap {
+    public static void main(String[] args) {
+        System.out.println("Hello world !?");
+    }
+
+    @CEntryPoint(name = "filter_env")
+    private static int filterEnv(IsolateThread thread, CCharPointer cFilter) {
+        String filter = CTypeConversion.toJavaString(cFilter);
+        Map<String, String> env = System.getenv();
+        int count = 0;
+        for (String envName : env.keySet()) {
+            if(!envName.contains(filter)) continue;
+            System.out.format("%s=%s%n",
+                    envName,
+                    env.get(envName));
+            count++;
+        }
+        return count;
+    }
+}
